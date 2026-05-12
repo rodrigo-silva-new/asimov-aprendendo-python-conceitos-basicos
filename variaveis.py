3d84fd4ef7df90b1a41fbb3d4b73b5138083a2da
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-05-12 16:29:20 -0300

    adiciona variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,22 @@
+print(3.14 * 5 ** 2)
+
+pi = 3.14
+raio = 5
+raio_ao_quadrado = raio ** 2
+
+print(pi * raio_ao_quadrado)
+
+
+
+
+
+
+
+
+
+
+x = 4
+print(x)
+
+meu_nome = 'Juliano'
+print(meu_nome)
