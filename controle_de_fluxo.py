c84ba7ec49b3ccbea765a993110b05906d6d1882
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-05-15 17:50:34 -0300

    sobe controle_de_fluxo.py

---
+++ controle_de_fluxo.py
@@ -0,0 +1,8 @@
+idade = int(input('Digite sua idade: '))
+
+if idade < 18:
+    print('Você tem menos de 18 anos')
+elif idade == 18:
+    print('Você tem exatamente 18 anos!')
+else:
+    print('Você tem mais de 18 anos')
