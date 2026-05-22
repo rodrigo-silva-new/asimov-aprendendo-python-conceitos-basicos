45bddf00404a2fd651475c7f89c0a69e136593b0
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-05-22 17:34:12 -0300

    sobe tuplas_e_sequencias.py

---
+++ 00-conceitos-basicos/tuplas_e_sequencias.py
@@ -0,0 +1,46 @@
+alunos = ['Marcos', 'Daniel', 'José']
+print(alunos)
+del alunos[0]
+print(alunos)
+
+
+valores = (1, 2, 3)
+print(valores)
+#del valores[0]
+#print(valores)
+
+
+nome = 'Juliano'
+print(nome[2])
+
+
+lista = [1, 2, 3, 4]
+print(tuple(lista))
+print(list(nome))
+
+
+
+lista_vazia = ''
+print(len(lista_vazia))
+
+
+
+
+seq = [1, 2, 3]
+if seq:
+    print('Sequência não está vazia')
+else:
+    print('Sequência está vazia')
+
+
+
+
+
+
+
+
+
+
+
+
+
