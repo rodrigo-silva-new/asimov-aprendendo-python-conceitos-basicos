508c9e4b82e48de7cef6b88b0b25a7a49ab93fd8
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-05-25 17:56:26 -0300

    sobe a_funcao_range.py

---
+++ 00-conceitos-basicos/a_funcao_range.py
@@ -0,0 +1,9 @@
+print(list(range(10)))
+print(list(range(1, 7)))
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numeros[1:5])
+print(list(range(1, 5)))
+print(numeros[0:10:2])
+print(list(range(0, 10, 2)))
