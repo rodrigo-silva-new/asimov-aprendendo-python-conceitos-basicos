893ca4a71a6621256119987334821940d504dea4
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-05-26 16:31:53 -0300

    sobe iterando_sobre_sequencias.py

---
+++ 00-conceitos-basicos/iterando_sobre_sequencias.py
@@ -0,0 +1,33 @@
+valores = [10, 20, 30, 3, -2, 17]
+
+for valor in valores:
+    print(f'O valor é: {valor}')
+
+print('Fim do primeiro loop')
+
+
+
+
+
+nome = 'Juliano'
+
+for caractere in nome:
+    print(f'O valor é: {caractere}')
+
+print('Fim do segundo loop')
+
+
+
+
+
+
+
+
+clientes = [('Ana', 'xxx', 'xxx@email.com'), ('João', 'yyy', 'yyy@email.com')]
+
+for nome, cpf, email in clientes:
+    print(f'Cliente: {nome}\nCPF: {cpf}\nEmail: {email}\n\n')
+
+
+
+
