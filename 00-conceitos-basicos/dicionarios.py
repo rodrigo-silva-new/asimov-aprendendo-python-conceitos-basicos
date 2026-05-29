8a41dfaa2ac546c1b89e49e1ccfd1f4839fe8392
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-05-29 17:43:06 -0300

    sobe dicionarios.py

---
+++ 00-conceitos-basicos/dicionarios.py
@@ -0,0 +1,9 @@
+capitais = {
+    'Brasil': 'Brasília',
+    'França': 'Paris',
+    'Japão': 'Tóquio'
+}
+
+for pais in capitais:
+    capital = capitais[pais]
+    print(f'A capital de {pais} é {capital}')
