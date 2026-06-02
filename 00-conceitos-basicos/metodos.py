d1757170f45b409bf0d2761ea0502bb9b4d76013
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-06-02 17:10:44 -0300

    sobe metodos.py

---
+++ 00-conceitos-basicos/metodos.py
@@ -0,0 +1,62 @@
+s = 'Olá Mundo!'
+
+num = 10
+
+dec = 3.33
+
+lista = [1, 2, 3]
+
+tupla = ('A', 'B', 'C')
+
+dic = {'chave1': 'valor1', 'chave2': 'valor2'}
+
+
+
+
+
+
+
+
+produtos = {
+    'banana': 3.60,
+    'leite': 4.90,
+    'carne': 15.50,
+    'pão': 9.00,
+}
+
+print(produtos)
+print(produtos.clear())
+print(dir(produtos))
+print(help(produtos.clear))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
