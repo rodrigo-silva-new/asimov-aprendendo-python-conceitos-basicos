658598eeee180ecbe7d9b57843d63f3bbaa8d0c5
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-06-20 19:30:47 -0300

    sobe funcoes.py

---
+++ 00-conceitos-basicos/funcoes.py
@@ -0,0 +1,31 @@
+def somar_dois(n):
+    return n + 2
+
+
+
+print(somar_dois(10))
+print(somar_dois(8))
+print(somar_dois(0))
+print(somar_dois(-3.5))
+
+
+
+
+
+def concatenar_texto(texto1, texto2):
+    return texto1 + texto2
+
+
+
+print(concatenar_texto('xxx', 'yyy'))
+
+
+
+
+
+
+
+
+
+
+
