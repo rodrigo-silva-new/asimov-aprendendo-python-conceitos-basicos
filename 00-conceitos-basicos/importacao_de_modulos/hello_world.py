1c55487d8fe3ace8726ae07865a72138fb64e506
Rodrigo Silva <rodrigonew290894@gmail.com>
2026-06-22 17:53:25 -0300

    sobe a pasta importacao_de_modulos

---
+++ 00-conceitos-basicos/importacao_de_modulos/hello_world.py
@@ -0,0 +1,11 @@
+import meu_modulo
+# from meu_modulo import minha_funcao, x
+# from meu_modulo import *
+# import meu_modulo as mm
+
+
+retorno = meu_modulo.minha_funcao()
+
+print(retorno)
+
+print(meu_modulo.x)
